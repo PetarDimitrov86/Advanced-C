967691cded9741768fb90db168f55216b77db24a
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-17 10:30:25 +0300

    Add files via upload

---
+++ Stacks-And-Queues/01-ReverseNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class ReverseNumbers
+{
+    static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        Stack<int> originalStack = new Stack<int>(arr);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(originalStack.Peek());
+            originalStack.Pop();
+            if (i != arr.Length - 1)
+                Console.Write(" ");
+            else
+                Console.WriteLine();
+        }
+    }
+}
