f002bc7f2b83f4ac65098d40f7682d0c9533e3de
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-18 12:04:25 +0300

    Add files via upload

---
+++ Stacks-And-Queues/09-StackFibonaci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class StackFibonaci
+{
+    static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+        Queue<long> fibQueue = new Queue<long>();
+        fibQueue.Enqueue(1);
+        fibQueue.Enqueue(1);
+        for (int i = 0; i < n - 1; i++)
+            fibQueue.Enqueue(fibQueue.Dequeue() + fibQueue.Peek());
+        Console.WriteLine(fibQueue.Peek());
+    }
+}
