77496b6ec39a3235720a86a16735e50d2550e27c
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-19 10:38:09 +0300

    Add files via upload

---
+++ SetsAndDictionaries/07-FixEmails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+class FixEmails
+{
+    static void Main(string[] args)
+    {
+    string name = Console.ReadLine();
+    string email = Console.ReadLine();
+    Dictionary<string, string> contacts = new Dictionary<string, string>();
+        Dictionary<string, string> result = new Dictionary<string, string>();
+    while (name != "stop")
+        {
+            if (!contacts.ContainsKey(name))
+                contacts[name] = email;
+            name = Console.ReadLine();
+            if (name == "stop")
+                break;
+            email = Console.ReadLine();
+        }
+
+        foreach (var kvp in contacts)
+        {
+            string emailValue = kvp.Value;
+            string[] emailarr = emailValue.Split('.');
+            string host = emailarr[emailarr.Length - 1];
+            if (host != "uk" && host != "us")
+                result.Add(kvp.Key, kvp.Value);
+        }
+        foreach (var kvp in result)
+            Console.WriteLine("{0} -> {1}", kvp.Key, kvp.Value);
+    }
+}
