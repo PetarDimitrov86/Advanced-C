4d174b648819fad696c1bade2b959ef7a5189c42
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-19 08:44:49 +0300

    Add files via upload

---
+++ SetsAndDictionaries/04-CountSymbols.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class CountSymbols
+{
+    static void Main(string[] args)
+    {
+        SortedDictionary<char, int> occurences = new SortedDictionary<char, int>();
+        string input = Console.ReadLine();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!occurences.ContainsKey(input[i]))
+                occurences[input[i]] = 0;
+            occurences[input[i]]++;
+        }
+        foreach (var kvp in occurences)
+            Console.WriteLine("{0}: {1} time/s", kvp.Key, kvp.Value);
+    }
+}
