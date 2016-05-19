34c5c6236a2754861498156d83ec9d7018d40ca1
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-19 08:53:46 +0300

    Add files via upload

---
+++ SetsAndDictionaries/05-Phonebook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+class Phonebook
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+        string input = Console.ReadLine();
+        while (input != "search")
+        {
+            string[] command = input.Split('-');
+            string name = command[0];
+            string number = command[1];
+            if (!phonebook.ContainsKey(name))
+                phonebook[name] = null;
+            phonebook[name] = number;
+            input = Console.ReadLine();
+        }
+        input = Console.ReadLine();
+        while (input != "stop")
+        {
+            if (phonebook.ContainsKey(input))
+                Console.WriteLine("{0} -> {1}", input, phonebook[input]);
+            else
+                Console.WriteLine("Contact {0} does not exist.", input);
+            input = Console.ReadLine();
+        }
+    }
+}
