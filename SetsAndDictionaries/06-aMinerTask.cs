ba6a89a64373cefd77799ff92a6a4c6c3ab8feaa
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-19 19:54:50 +0300

    Add files via upload

---
+++ SetsAndDictionaries/06-aMinerTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class aMinerTask
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, decimal> resources = new Dictionary<string, decimal>();
+        string material = Console.ReadLine();
+        string quantity = Console.ReadLine();
+        while (material != "stop")
+        {
+            if (!resources.ContainsKey(material))
+                resources[material] = 0;
+            resources[material] += decimal.Parse(quantity);
+            material = Console.ReadLine();
+            if (material == "stop")
+                break;
+            quantity = Console.ReadLine();
+        }
+        foreach (var kvp in resources)
+             Console.WriteLine("{0} -> {1}", kvp.Key, kvp.Value);
+    }
+}
