031484b95f1357476861df300108c5ef00cf956f
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-19 08:39:28 +0300

    Add files via upload

---
+++ SetsAndDictionaries/03-PeriodicTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class PeriodicTable
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        SortedSet<string> uniqueCompounds = new SortedSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] compounds = Console.ReadLine().Split(' ');
+            for (int j = 0; j < compounds.Length; j++)
+                uniqueCompounds.Add(compounds[j]);
+        }
+        Console.WriteLine(string.Join(" ", uniqueCompounds));
+    }
+}
