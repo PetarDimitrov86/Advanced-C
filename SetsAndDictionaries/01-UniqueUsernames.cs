3825cca88c309ceef5f9a4c0e6e078656ed4a793
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-19 08:31:05 +0300

    Add files via upload

---
+++ SetsAndDictionaries/01-UniqueUsernames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class UniqueUsernames
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> hash = new HashSet<string>();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            hash.Add(input);
+        }
+        foreach (var item in hash)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
