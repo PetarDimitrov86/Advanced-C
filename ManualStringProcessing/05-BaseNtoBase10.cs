ee22f33404772bcb9adb35315c286611fd235238
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-31 08:36:36 +0300

    Add files via upload

---
+++ ManualStringProcessing/05-BaseNtoBase10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+class BaseNtoBase10
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ').ToArray();
+        BigInteger baseNum = BigInteger.Parse(input[0]);
+        string numberToConv = input[1];
+        BigInteger result = BigInteger.Parse(numberToConv[0].ToString());
+        for (int i = 0; i < numberToConv.Length - 1; i++)
+        {
+            BigInteger numToAdd = result * baseNum + BigInteger.Parse(numberToConv[i + 1].ToString());
+            result = numToAdd;
+        }
+        Console.WriteLine(result);
+    }
+}
