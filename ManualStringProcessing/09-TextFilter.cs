6921ac8e830b1df2603256cf2ff1fe63e249ad00
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-31 14:31:06 +0300

    Add files via upload

---
+++ ManualStringProcessing/09-TextFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+class TextFilter
+{
+    static void Main(string[] args)
+    {
+        string[] badWords = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries ).ToArray();
+        string input = Console.ReadLine();
+        string result = String.Empty;
+        foreach (var item in badWords)
+        {
+            result = input.Replace(item, new string('*', item.Length));
+            input = result;
+        }
+        Console.WriteLine(result);
+    }
+}
