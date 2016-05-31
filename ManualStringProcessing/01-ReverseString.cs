b4e0828c0ebc7d9a6a3412debd4628ef569f47df
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-31 08:10:40 +0300

    Add files via upload

---
+++ ManualStringProcessing/01-ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ReverseString
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        StringBuilder result = new StringBuilder();
+        for (int i = input.Length - 1; i >= 0; i--)
+            result.Append(input[i]);
+        Console.WriteLine(result);
+    }
+}
