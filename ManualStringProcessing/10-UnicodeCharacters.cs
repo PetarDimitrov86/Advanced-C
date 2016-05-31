bd01daa7492b40233fbdc8488f12ff19d01e0567
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-31 14:52:01 +0300

    Add files via upload

---
+++ ManualStringProcessing/10-UnicodeCharacters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+class UnicodeCharacters
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            result.Append("\\u");
+            result.Append(string.Format("{0:x4}", (int)input[i]));
+        }
+        Console.WriteLine(result);
+    }
+}
