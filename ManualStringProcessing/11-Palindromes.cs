6c0b8b63a89b926960646757e657e3529d40c1b1
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-31 15:23:40 +0300

    Add files via upload

---
+++ ManualStringProcessing/11-Palindromes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Palindromes
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(new[] { ',', '.', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+        SortedSet<string> result = new SortedSet<string>();
+        foreach (var item in input)
+        {
+            if (IsSymetrical(item))
+                result.Add(item);
+        }
+        Console.WriteLine("[{0}]", string.Join(", ", result));
+    }
+    public static bool IsSymetrical(string input)
+    {
+        string leftPart = string.Empty;
+        string rightPart = string.Empty;
+        for (int i = 0; i < input.Length / 2; i++)
+            leftPart += input[i];
+        for (int i = input.Length - 1; i >= (input.Length + 1) / 2; i--)
+            rightPart += input[i];
+        if (leftPart == rightPart)
+            return true;
+        return false;
+    }
+}
