70e37a3b7297289b34e9fbae1c746a6f59449725
Petar Dimitrov <lbmikesmith@gmail.com>
2016-05-31 08:51:10 +0300

    Add files via upload

---
+++ ManualStringProcessing/06-CountSubstringOccurences.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class CountSubstringOccurences
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine().ToLower();
+        string word = Console.ReadLine().ToLower();
+        int counter = 0;
+        int wordLength = word.Length;
+        for (int i = 0; i < input.Length - wordLength + 1; i++)
+        {
+            if (input.Substring(i, wordLength) == word)
+                counter++;
+        }
+        Console.WriteLine(counter);
+    }
+}
