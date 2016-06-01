4588b611c661bc439276fbae2783a3a39b120080
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-01 15:30:01 +0300

    Add files via upload

---
+++ ManualStringProcessing/02-StringLength.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class StringLength
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        if (input.Length < 21)
+        {
+            Console.Write(input);
+            Console.WriteLine(new string('*', 20 - input.Length));
+        }
+        else
+            Console.WriteLine(input.Substring(0, 20));
+    }
+}
