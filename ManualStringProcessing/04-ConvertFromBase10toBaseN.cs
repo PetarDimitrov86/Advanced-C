4f299564c813538e9d9d0467535e13f8517cf8ae
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-01 15:31:06 +0300

    Add files via upload

---
+++ ManualStringProcessing/04-ConvertFromBase10toBaseN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+class ConvertFromBase10toBaseN
+{
+    static void Main(string[] args)
+    {
+        BigInteger[] nums = Console.ReadLine().Split(' ').Select(BigInteger.Parse).ToArray();
+        BigInteger baseNum = nums[0];
+        BigInteger number = nums[1];
+        BigInteger result = 0;
+        BigInteger counter = 1;
+        while (number > 0)
+        {
+            result += (number % baseNum) * counter;
+            number /= baseNum;
+            counter *= 10;
+        }
+        Console.WriteLine(result);
+    }
+}
