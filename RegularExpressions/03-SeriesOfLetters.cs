918c8a542ef0bc125497ff1edffa3755eb6aa2a4
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-02 12:16:44 +0300

    Add files via upload

---
+++ RegularExpressions/03-SeriesOfLetters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+class SeriesOfLetters
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        HashSet<char> letters = new HashSet<char>();
+        for (int i = 0; i < text.Length; i++)
+            letters.Add(text[i]);
+        foreach (var item in letters)
+        {
+            string pattern = item + @"{2,}";
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(text, item.ToString());
+            text = result;
+        }
+        Console.WriteLine(text);
+    }
+}
