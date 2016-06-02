f6c895546c743ad0cff441d1c9d42effdc449145
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-02 09:55:01 +0300

    Add files via upload

---
+++ RegularExpressions/01-MatchFullName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class MatchFullName
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        string pattern = @"\b([A-Z][a-z]+ [A-Z][a-z]+)\b";
+        Regex regex = new Regex(pattern);
+        Match match = regex.Match(text);
+        while (text != "end")
+        {
+            if (regex.IsMatch(text))
+                Console.WriteLine(regex.Match(text));
+            text = Console.ReadLine();
+        }
+    }
+}
