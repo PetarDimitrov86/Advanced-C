00794a6268750eec19f8d60882bf8b2fcf520ad1
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-02 11:17:44 +0300

    Add files via upload

---
+++ RegularExpressions/02-MatchPhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class MatchPhoneNumber
+{
+    static void Main(string[] args)
+    {
+        string number = Console.ReadLine();
+        string pattern = @"^\+359( |-)2{1}\1\d{3}\1\d{4}\b";
+        Regex regex = new Regex(pattern);
+        while (number != "end")
+        {
+            if (regex.IsMatch(number))
+                Console.WriteLine(number);
+            number = Console.ReadLine();
+        }
+
+    }
+}
