1ccf1220060cafca69607b6634cda1fedbcd5e43
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-03 20:38:32 +0300

    Add files via upload

---
+++ ManualStringProcessing/15-MelrahShake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class MelrahShake
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        string pattern = Console.ReadLine();
+        while (text.Contains(pattern) && text.IndexOf(pattern) != text.LastIndexOf(pattern) && pattern.Length > 0)
+        {
+            int leftOne = text.IndexOf(pattern);
+            text = text.Remove(leftOne, pattern.Length);
+            int rightOne = text.LastIndexOf(pattern);
+            text = text.Remove(rightOne, pattern.Length);
+            pattern = pattern.Remove(pattern.Length/2, 1);
+            Console.WriteLine("Shaked it.");
+        }
+        Console.WriteLine("No shake.");
+        Console.WriteLine(text);
+    }
+}
