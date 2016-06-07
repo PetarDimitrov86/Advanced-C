96f95a7715f710a935505b7611fcc9d31b72ac5f
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-07 12:10:18 +0300

    Add files via upload

---
+++ FunctionalProgramming/02-KnightsOfHonor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+class KnightsOfHonor
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        Action<string> print = name => Console.WriteLine("Sir " + name);
+        foreach (var item in input)
+            print(item);
+    }
+}
