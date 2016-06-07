ba5feb8814ce6708ec0e07c8472aca0371b37b73
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-07 12:02:42 +0300

    Add files via upload

---
+++ FunctionalProgramming/01-ActionPrint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class ActionPrint
+{
+    static void Main(string[] args)
+    {
+        string[] text = Console.ReadLine().Split(' ').ToArray();
+        Action<string> print = message => Console.WriteLine(message);
+        foreach (var item in text)
+            print(item);
+    }
+}
