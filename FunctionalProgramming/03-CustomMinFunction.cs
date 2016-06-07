8671dfdbf03bcf793bf59b79799a0eb88b17ff7b
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-07 12:19:59 +0300

    Add files via upload

---
+++ FunctionalProgramming/03-CustomMinFunction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+class CustomMinFunction
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        Func<int[], int> result = number => numbers.Min();
+        Console.WriteLine(result(numbers));
+    }
+}
