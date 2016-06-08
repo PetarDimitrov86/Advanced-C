f71d949875639643ef4031b6a5681681ff104b21
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-08 23:18:07 +0300

    Add files via upload

---
+++ LINQ/04-SortStudents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Students
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+public static class SortStudents
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        List<Students> names = new List<Students>();
+        while (input != "END")
+        {
+            string[] command = input.Split();
+            Students tempStud = new Students();
+            tempStud.FirstName = command[0];
+            tempStud.LastName = command[1];
+            names.Add(tempStud);
+            input = Console.ReadLine();
+        }
+        var result = names.OrderBy(x => x.LastName).ThenByDescending(x => x.FirstName);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item.FirstName + " " + item.LastName);
+        }
+    }
+}
