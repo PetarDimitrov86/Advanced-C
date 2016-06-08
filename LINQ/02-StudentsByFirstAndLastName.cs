7683a07ec690b3fcbefc356484a65bf4680026f9
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-08 23:02:17 +0300

    Add files via upload

---
+++ LINQ/02-StudentsByFirstAndLastName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Students
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+class StudentsByFirstAndLastName
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        List<Students> names = new List<Students>();
+        while (input != "END")
+        {
+            string[] command = input.Split();
+            Students tempStud = new Students();
+            tempStud.FirstName = command[0];
+            tempStud.LastName = command[1];
+            names.Add(tempStud);
+            input = Console.ReadLine();
+        }
+        var result = names.Where(x => x.LastName.CompareTo(x.FirstName) == 1);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item.FirstName + " " + item.LastName);
+        }
+    }
+}
