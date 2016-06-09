13baa95297f56aa56a47b483f477dca61ec5a156
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-09 13:50:32 +0300

    Add files via upload

---
+++ LINQ/01-StudentsByGroupVer2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class StudentsByGroup
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        List<string> nameList = new List<string>();
+        while (input != "END")
+        {
+            nameList.Add(input);  
+            input = Console.ReadLine();
+        }
+        var result = nameList.Select(x => x.Split()).Where(x => x.Contains("2")).OrderBy(x => x[0]);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item[0] + " " + item[1]);
+        }
+    }
+}
