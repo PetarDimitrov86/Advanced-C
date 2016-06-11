dc298cd72f4c927980c52d4a6abf3515b42c7996
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-11 11:33:45 +0300

    only 66/100

---
+++ RegularExpressions/04-ReplaceATag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class ReplaceATag
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string pattern = @"(<ahref="")(\S*)(\"">)(\w*)(<\/a>)";
+        Regex regex = new Regex(pattern);
+        Match match = regex.Match(input);
+        string replacement = @"[URL href=""" + match.Groups[2].Value + "\"]" + match.Groups[4].Value + "[/URL]";
+        string result = regex.Replace(input, replacement);
+        Console.WriteLine(result);
+    }
+}
