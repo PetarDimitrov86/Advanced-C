1baabc5a15db4ba340123e65a1c0c8f9387e5afe
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-12 17:26:19 +0300

    Add files via upload

---
+++ RegularExpressions/07-ValidUsernames.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+class ValidUsernames
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string pattern = @"\b([a-zA-Z]\w{2,24})[\\\/\)\( ]?\b";
+        Regex regex = new Regex(pattern);
+        int sum = 0;
+        int biggestSum = int.MinValue;
+        MatchCollection matches = regex.Matches(input);
+        List<string> result = new List<string>();
+        string pairUsernames = string.Empty;
+        foreach (Match item in matches)
+        {
+            pairUsernames += item.Groups[1] + "(";
+        }
+        string[] usernames = pairUsernames.Split(new char[] { '\\', '/', '(', ')' });
+        for (int i = 0; i < usernames.Length - 1; i++)
+        {
+            string firstUsername = usernames[i];
+            string secondUsername = usernames[i + 1];
+            sum = firstUsername.Length + secondUsername.Length;
+            if (sum > biggestSum)
+            {
+                result.Clear();
+                result.Add(usernames[i]);
+                result.Add(usernames[i+1]);
+                biggestSum = sum;
+            }
+            sum = 0;
+        }
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
