c44e7606589981a949306424adf2d3959934f782
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-12 11:19:50 +0300

    Add files via upload

---
+++ RegularExpressions/05-ExtractEmail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class ExtractEmail
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string pattern = @"(?:^|\s)((?:[a-zA-Z0-9]+[.\-_])*[a-zA-Z0-9]+@(?:[a-zA-Z]+-?)+[a-zA-Z](?:\.[a-zA-Z]+)+)(?:\.\s)?";
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(input);
+        foreach (Match match in matches)
+            Console.WriteLine(match.Groups[0]);
+    }
+}
