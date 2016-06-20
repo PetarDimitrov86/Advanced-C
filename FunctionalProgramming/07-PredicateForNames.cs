2e7b19fa9580117a61c3dc98df751ac2260df165
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-20 13:47:04 +0300

    Add files via upload

---
+++ FunctionalProgramming/07-PredicateForNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+class PredicateForNames
+{
+    static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split(' ').ToArray();
+        Predicate<string> predic = x => x.Length <= length;
+        foreach (var name in names)
+        {
+            if (predic(name))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
