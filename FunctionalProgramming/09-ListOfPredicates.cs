0cad6bed6c59b5b9583bd8e6fa7507695e708189
Petar Dimitrov <lbmikesmith@gmail.com>
2016-06-20 16:18:44 +0300

    Add files via upload

---
+++ FunctionalProgramming/09-ListOfPredicates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class ListOfPredicates
+{
+    static void Main(string[] args)
+    {
+        int end = int.Parse(Console.ReadLine());
+        int start = 1;
+        List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).Distinct().ToList();
+        List<Predicate<int>> predList = new List<Predicate<int>>();
+        foreach (var num in nums)
+        {
+            predList.Add(x => x % num != 0);
+        }
+        bool IsValid = true;
+        for (int k = start; k <= end; k++)
+        {
+            IsValid = true;
+            for (int j = 0; j < predList.Count; j++)
+            {
+                if (predList[j](k))
+                {
+                    IsValid = false;
+                    break;
+                }
+            }
+            if (IsValid)
+            {
+                Console.Write(k + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
